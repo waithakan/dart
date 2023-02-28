ebe20888bea7b3ebb3db4960b11f797c023e17fb
waithakan <waithakadylan@gmail.com>
2023-02-28 19:40:49 +0300

    object example

---
+++ class.dart
@@ -0,0 +1,23 @@
+class Animal {
+  String? name; //the bquestion mark is for over
+  int? numberOfLegs;
+  int? lifespan;
+
+  void display() {
+    print("Animal name: $name");
+    print("Number of legs: $numberOfLegs");
+    print("life span: $lifespan");
+  }
+
+}
+void main() {
+    Animal animal = Animal();
+    animal.name = "Dog";
+    animal.numberOfLegs = 4;
+    animal.lifespan = 10;
+    animal.display();
+  }
+
+
+
+
